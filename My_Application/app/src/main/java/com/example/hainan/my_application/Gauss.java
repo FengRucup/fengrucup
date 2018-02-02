cc4e3f665f7e3cc25c4545ee87edc30972fa7320
hainan <15061103@buaa.edu.cn>
2018-02-02 18:40:12 +0800

    first

---
+++ My_Application/app/src/main/java/com/example/hainan/my_application/Gauss.java
@@ -0,0 +1,343 @@
+package com.example.hainan.my_application;
+
+
+import android.support.annotation.NonNull;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Gauss {
+    public Map gaussmix(float x[][],float c,float l,int m0,String v0){
+        return new Map() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object o) {
+                return false;
+            }
+
+            @Override
+            public Object get(Object o) {
+                return null;
+            }
+
+            @Override
+            public Object put(Object o, Object o2) {
+                return null;
+            }
+
+            @Override
+            public Object remove(Object o) {
+                return null;
+            }
+
+            @Override
+            public void putAll(@NonNull Map map) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @NonNull
+            @Override
+            public Set keySet() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Collection values() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Set<Entry> entrySet() {
+                return null;
+            }
+        };
+    }
+    public Map gaussmix(float x[][],float c,float l,float m0[][],float v0[][],float w0[]){
+        return new Map() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object o) {
+                return false;
+            }
+
+            @Override
+            public Object get(Object o) {
+                return null;
+            }
+
+            @Override
+            public Object put(Object o, Object o2) {
+                return null;
+            }
+
+            @Override
+            public Object remove(Object o) {
+                return null;
+            }
+
+            @Override
+            public void putAll(@NonNull Map map) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @NonNull
+            @Override
+            public Set keySet() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Collection values() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Set<Entry> entrySet() {
+                return null;
+            }
+        };
+    }
+    public Map gaussmixp(float y[][],float m[][],float v[][],float w[]){
+        return new Map() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object o) {
+                return false;
+            }
+
+            @Override
+            public Object get(Object o) {
+                return null;
+            }
+
+            @Override
+            public Object put(Object o, Object o2) {
+                return null;
+            }
+
+            @Override
+            public Object remove(Object o) {
+                return null;
+            }
+
+            @Override
+            public void putAll(@NonNull Map map) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @NonNull
+            @Override
+            public Set keySet() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Collection values() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Set<Entry> entrySet() {
+                return null;
+            }
+        };
+    }
+    private Map gaumix_train_diagonal(float xs[][],float m[][],float v[][],float w[],float c,float l,float sx0[],float mx0[]){
+        return new Map() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object o) {
+                return false;
+            }
+
+            @Override
+            public Object get(Object o) {
+                return null;
+            }
+
+            @Override
+            public Object put(Object o, Object o2) {
+                return null;
+            }
+
+            @Override
+            public Object remove(Object o) {
+                return null;
+            }
+
+            @Override
+            public void putAll(@NonNull Map map) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @NonNull
+            @Override
+            public Set keySet() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Collection values() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Set<Entry> entrySet() {
+                return null;
+            }
+        };
+    }
+    public Map kmeans(float d[][],int k,String x0,int l){
+        return new Map() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object o) {
+                return false;
+            }
+
+            @Override
+            public Object get(Object o) {
+                return null;
+            }
+
+            @Override
+            public Object put(Object o, Object o2) {
+                return null;
+            }
+
+            @Override
+            public Object remove(Object o) {
+                return null;
+            }
+
+            @Override
+            public void putAll(@NonNull Map map) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @NonNull
+            @Override
+            public Set keySet() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Collection values() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Set<Entry> entrySet() {
+                return null;
+            }
+        };
+    }
+
+}
