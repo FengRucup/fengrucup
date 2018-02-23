c41ca58dc3f454b0bd16031442c84f007920917a
lreaderl <tonyyy@foxmail.com>
2018-02-23 20:09:02 +0800

    wave files

---
+++ My_Application/app/src/main/java/com/example/hainan/my_application/RecordStreamListener.java
@@ -0,0 +1,9 @@
+package com.example.hainan.my_application;
+
+/**
+ * Created by linto on 2018/2/23.
+ */
+
+public interface RecordStreamListener {
+    void recordOfByte(byte[] data, int begin, int end);
+}
